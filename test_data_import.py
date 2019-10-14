35079a272a7eb322ee8edb85d79850361fc146e2
root <root@Unbound.localdomain>
2019-10-14 15:20:33 -0600

    corrected empty value import check

--- test_data_import.py
+++ test_data_import.py
@@ -2,7 +2,9 @@ import unittest
 import random
 import sys
 import os.path
-import data_import as datimp
+import datetime
+import dateutil.parser
+from data_import import ImportData as idata
 from os import path
 
 
@@ -10,10 +12,20 @@ class TestDataImport(unittest.TestCase):
     def testImportDataEmptyValues(self):
         test_filename = "test_file"
         test_file = open(test_filename, "w")
-        file.write("testtime, testvals \n")
-        for n in range(0: 100):
-            file.write("test" + str(n) + " " + 1 + "\n")
-        for n in range(0: 10):
-            file.write("test" + str(n+100) + "   \n")
-        testobject = data_import(test_filename)
+        test_file.write("time,value\n")
+        for n in range(0, 100):
+            tempdate = str(datetime.datetime.now())
+            test_file.write(tempdate + "," + str(1) + "\n")
+
+        for n in range(0, 10):
+            tempdate = str(datetime.datetime.now())
+            test_file.write(tempdate + "," + "\n")
+
+        test_file.close()
+        testobject = idata(test_filename)
         self.assertEqual(100, len(testobject._value))
+        test_filename
+
+
+if __name__ == '__main__':
+    unittest.main()
