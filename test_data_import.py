6194b3ffe2307e9c0d62d1f4772fff5757bc8a92
root <root@Unbound.localdomain>
2019-10-14 16:06:05 -0600

    finalized test for highlow vals

--- test_data_import.py
+++ test_data_import.py
@@ -24,17 +24,18 @@ class TestDataImport(unittest.TestCase):
         test_file.close()
         testobject = idata(test_filename)
         self.assertEqual(100, len(testobject._value))
-        test_filename
-    
+
     def testImportDataHighLow(self):
         test_filename = "test_file"
         test_file = open(test_filename, "w")
         test_file.write("time,value\n")
         testarray = []
-        for n in range(0,20):
+        for n in range(0, 100):
+            testarray.append(1)
+        for n in range(0, 20):
             if(n < 10):
                 testarray.append(300)
-            if(n > 10):
+            if(n >= 10):
                 testarray.append(40)
 
         for n in range(0, 100):
@@ -51,7 +52,8 @@ class TestDataImport(unittest.TestCase):
         test_file.close()
         testobject = idata(test_filename)
 
-        self.assertEqual(testarray, testobject._value[100 : 120])
-        
+        self.assertEqual(testarray, testobject._value[0: 120])
+
+
 if __name__ == '__main__':
     unittest.main()
